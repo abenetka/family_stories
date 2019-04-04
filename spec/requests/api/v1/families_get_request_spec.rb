8e7e8e63c9448c8e6be5ac432c9b9a8f2f49113c
abenetka <ali.benetka@gmail.com>
2019-04-04 16:08:28 -0600

    Updates get families request spec

--- spec/requests/api/v1/families_get_request_spec.rb
+++ spec/requests/api/v1/families_get_request_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe 'Families API' do
   it "returns all families" do
+    create_list(:family, 2)
+    get '/api/v1/families'
 
-  get '/api/v1/families'
+    expect(response).to be_successful
 
-  expect(response).to be_successful
+    families = JSON.parse(response.body)
+    expect(families.count).to eq(2)
   end
 end
