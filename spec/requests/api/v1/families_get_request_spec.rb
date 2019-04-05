1714f0a9b433667c55982c3d19307506ba25c637
Leah Miller <lkmiller.87@gmail.com>
2019-04-04 18:52:01 -0600

    Adds test for find families by id

--- spec/requests/api/v1/families_get_request_spec.rb
+++ spec/requests/api/v1/families_get_request_spec.rb
@@ -10,4 +10,14 @@ describe 'Families API' do
     families = JSON.parse(response.body)
     expect(families.count).to eq(2)
   end
+
+  it "can get one family by its id" do
+      id = create(:family).id
+
+      get "/api/v1/families/#{id}"
+      family = JSON.parse(response.body)
+      
+      expect(response).to be_successful
+      expect(family["id"]).to eq(id)
+  end
 end
