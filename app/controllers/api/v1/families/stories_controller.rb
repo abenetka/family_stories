6cc8a9602f9c1833cb1fc4c193ca7e31b6566fa0
Leah Miller <lkmiller.87@gmail.com>
2019-04-06 17:31:09 -0600

    Adds create method for stories within stories controller

--- app/controllers/api/v1/families/stories_controller.rb
+++ app/controllers/api/v1/families/stories_controller.rb
@@ -8,4 +8,12 @@ class Api::V1::Families::StoriesController < ApplicationController
     end
   end
 
+  def create
+    render json: Story.create(story_params)
+  end
+
+  private
+  def story_params
+    params.require(:story).permit(:title, :content, :author, :family_id)
+  end
 end
