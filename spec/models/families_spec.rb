8748e5175405d973d0acaf916fedd75562233d92
Leah Miller <lkmiller.87@gmail.com>
2019-04-06 15:08:19 -0600

    Adds relationship expectations to the families model test

--- spec/models/families_spec.rb
+++ spec/models/families_spec.rb
@@ -4,6 +4,10 @@ describe Family, type: :model do
   describe 'relationships' do
     it { should have_many :user_families }
     it { should have_many(:users).through(:user_families) }
+
+      it { should have_many :stories }
+      it { should have_many :recipes }
+      it { should have_many :photos }
   end
 
 end
