c2c0c3c155ce218fd003a9ca7937b36c1ae24eba
Leah Miller <lkmiller.87@gmail.com>
2019-04-06 13:21:34 -0600

    Adds recipes index route nested within families

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :families do
+        get "/:id/recipes", to: 'recipes#index'
+      end
       resources :families, only: [:index, :show]
     end
   end
