0cee2909c693c6aa073323884606873cd3c96763
Leah Miller <lkmiller.87@gmail.com>
2019-04-06 17:30:51 -0600

    Adds post request to routes for stories

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :families do
         get '/:id/stories', to: 'stories#index'
+        post '/:id/stories', to: 'stories#create'
         get "/:id/recipes", to: 'recipes#index'
         get "/:id/photos", to: 'photos#index'
       end
