9455c36901c7c5def0c5421df5e71d8ec96cf795
Leah Miller <lkmiller.87@gmail.com>
2019-04-06 14:10:08 -0600

    Adds updates to test setup for factorybot

--- spec/requests/api/v1/recipes_get_request_spec.rb
+++ spec/requests/api/v1/recipes_get_request_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 describe 'Recipes API' do
   it "returns all recipes for one family" do
-    id = create(:family, id: 1).id
-    family = create(:family, id: 2)
-    create_list(:recipe, 3, family_id: id)
-    create_list(:recipe, 2, family_id: family.id)
+    family_1 = create(:family)
+    family_2 = create(:family)
+    create_list(:recipe, 3, family: family_1)
+    create_list(:recipe, 2, family: family_2)
 
-    get "/api/v1/families/#{id}/recipes"
+    get "/api/v1/families/#{family_1.id}/recipes"
 
     expect(response).to be_successful
 
     recipes = JSON.parse(response.body)["data"]
     expect(recipes.count).to eq(3)
-    expect(recipes[0]["relationships"]["family"]["data"]["id"]).to eq(id.to_s)
-    expect(recipes[1]["relationships"]["family"]["data"]["id"]).to eq(id.to_s)
-    expect(recipes[2]["relationships"]["family"]["data"]["id"]).to eq(id.to_s)
-    expect(recipes[0]["relationships"]["family"]["data"]["id"]).to_not eq(family.id.to_s)
-    expect(recipes[1]["relationships"]["family"]["data"]["id"]).to_not eq(family.id.to_s)
-    expect(recipes[2]["relationships"]["family"]["data"]["id"]).to_not eq(family.id.to_s)
+    expect(recipes[0]["relationships"]["family"]["data"]["id"]).to eq(family_1.id.to_s)
+    expect(recipes[1]["relationships"]["family"]["data"]["id"]).to eq(family_1.id.to_s)
+    expect(recipes[2]["relationships"]["family"]["data"]["id"]).to eq(family_1.id.to_s)
+    expect(recipes[0]["relationships"]["family"]["data"]["id"]).to_not eq(family_2.id.to_s)
+    expect(recipes[1]["relationships"]["family"]["data"]["id"]).to_not eq(family_2.id.to_s)
+    expect(recipes[2]["relationships"]["family"]["data"]["id"]).to_not eq(family_2.id.to_s)
   end
   it "returns no recipes if none exist" do
     id = create(:family).id
