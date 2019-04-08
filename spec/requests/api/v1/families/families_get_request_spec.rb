7363e2145a8a67a9d2707bee068e4c482d2a6c7d
abenetka <ali.benetka@gmail.com>
2019-04-08 13:41:12 -0600

    Adds sad path testing for post/create families

--- spec/requests/api/v1/families/families_get_request_spec.rb
+++ spec/requests/api/v1/families/families_get_request_spec.rb
@@ -32,4 +32,16 @@ describe 'Families API' do
     expect(family.name).to eq(family_params[:name])
   end
 
+  it "wont create a family with missing information" do
+    family_params = { not_a_name: ""}
+    
+    post "/api/v1/families", params: {family: family_params }
+
+    family = Family.last
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(response.body).to eq("Oops, you forgot some information!")
+  end
+
 end
