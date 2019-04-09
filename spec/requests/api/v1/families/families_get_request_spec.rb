3858feaf0517225cebc3e7efbe4fb3d63c4b159a
Leah Miller <lkmiller.87@gmail.com>
2019-04-09 11:38:40 -0600

    Removes post request from get request spec test file

--- spec/requests/api/v1/families/families_get_request_spec.rb
+++ spec/requests/api/v1/families/families_get_request_spec.rb
@@ -20,28 +20,4 @@ describe 'Families API' do
       expect(response).to be_successful
       expect(family["id"]).to eq(id)
   end
-
-  it "can create a family" do
-    family_params = { name: "Fay" }
-
-    post "/api/v1/families", params: family_params
-
-    family = Family.last
-
-    expect(response).to be_successful
-    expect(family.name).to eq(family_params[:name])
-  end
-
-  it "wont create a family with missing information" do
-    family_params = { not_a_name: ""}
-
-    post "/api/v1/families", params: family_params 
-
-    family = Family.last
-
-    expect(response).to_not be_successful
-    expect(response.status).to eq(401)
-    expect(response.body).to eq("Oops, you forgot some information!")
-  end
-
 end
